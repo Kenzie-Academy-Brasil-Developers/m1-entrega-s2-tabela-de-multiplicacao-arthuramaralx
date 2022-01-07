133e0c5086568f5dfc52b35eeca4b82eb1b611a7
Arthuramaralx <Arthuramaralx@outlook.com>
2022-01-07 02:16:22 -0300

    commit para entrega

---
+++ tabela.js
@@ -0,0 +1,17 @@
+function tabelaDeMultiplicação (num){
+    let tabela = []
+  let numMultiplicado;
+
+    for (let i = 0; i <=num; i++){
+      let linha = [];
+        for (let j = 0; j <=num; j++){
+           numMultiplicado =  i * j ;
+         linha.push(numMultiplicado)
+        }
+  
+        tabela.push(linha)
+
+    }
+    return tabela
+}
+//console.table(tabelaDeMultiplicação(16))

395e86863ed1d1793dc216e4f6a92a36a56650e2
Arthuramaralx <Arthuramaralx@outlook.com>
2022-01-11 10:38:53 -0300

    ultimo commit

--- tabela.js
+++ tabela.js
@@ -1,17 +1,20 @@
-function tabelaDeMultiplicação (num){
-    let tabela = []
-  let numMultiplicado;
 
-    for (let i = 0; i <=num; i++){
-      let linha = [];
-        for (let j = 0; j <=num; j++){
-           numMultiplicado =  i * j ;
-         linha.push(numMultiplicado)
-        }
-  
-        tabela.push(linha)
 
-    }
-    return tabela
+function tabelaDeMultiplicacao (num){
+let tabela = []
+let numMultiplicado;
+
+  for (let i = 0; i <=num; i++){
+    let linha = [];
+      for (let j = 0; j <=num; j++){
+         numMultiplicado =  i * j ;
+       linha.push(numMultiplicado)
+      }
+
+      tabela.push(linha)
+  }
+
+   console.table(tabela)
+
 }
-console.table(tabelaDeMultiplicação(16))
+console.log(tabelaDeMultiplicacao(16))
